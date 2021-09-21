5f1cd2175df0c8046f8cbb6dd188cda4e0b0e6f7
Navi Kaur <navdeep9520@gmail.com>
2021-09-21 16:03:21 -0700

    commit tree 57bd8c3fe6efed0a288401a34ceaf0f7c65e6157

---
+++ index.js
@@ -0,0 +1,16 @@
+const divide= () => 2000 / 100 ; 
+
+
+// 
+// const divide = 
+console.log (divide)
+
+const square = (num1) => num1 * num1;
+
+const add = (sum1,sum2) => sum1 + sum2;
+
+
+// const divide = function(){
+//     const divide = 2000 / 100;
+//     return divide; 
+//     }
